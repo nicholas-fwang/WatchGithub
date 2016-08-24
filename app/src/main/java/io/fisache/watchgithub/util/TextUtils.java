88fbfec3bdc651925c0275707c635106420f3e4e
fisache <hwang031451@gamil.com>
2016-08-24 11:38:21 +0900

    Edit fork and star k calculation

--- app/src/main/java/io/fisache/watchgithub/util/TextUtils.java
+++ app/src/main/java/io/fisache/watchgithub/util/TextUtils.java
@@ -4,13 +4,13 @@ public class TextUtils {
     public static String getForkStarString(int fork, int star) {
         String ret = "";
         if(fork >= 1000) {
-            ret += fork/1000 + "." + fork/100 + "k";
+            ret += fork/1000 + "." + (fork%1000)/100 + "k";
         } else {
             ret += fork;
         }
         ret += " forked, ";
         if(star >= 1000) {
-            ret += star/1000 + "." + star/100 + "k";
+            ret += star/1000 + "." + (star%1000)/100 + "k";
         } else {
             ret += star;
         }
